b271b504e90b3974536522b3cb96c201477f5898
Nikita Remeslov <creatorbon@gmail.com>
2019-07-27 20:19:41 +0300

    Updates

---
+++ precache-manifest.6290c71ccd2e4945bbe2172c0e117b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dcdbb368087b55d553b819b8c532ff",
+    "url": "/react-test-task/index.html"
+  },
+  {
+    "revision": "b8d8e60bf3a1cc8db0f3",
+    "url": "/react-test-task/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f07f62d7f3ffdd743478",
+    "url": "/react-test-task/static/js/2.61cb5f44.chunk.js"
+  },
+  {
+    "revision": "b8d8e60bf3a1cc8db0f3",
+    "url": "/react-test-task/static/js/main.7b5f314a.chunk.js"
+  },
+  {
+    "revision": "5ead977a24995f39964b",
+    "url": "/react-test-task/static/js/runtime~main.bc83e913.js"
+  }
+]);
